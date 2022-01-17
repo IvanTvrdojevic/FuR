79e95e83b70d2790aec2b09ced8f43eadaeb84d6
unknown <ivan.tvrdojevic@gmail.com>
2022-01-17 13:35:56 +0100

    moving forms to separate folder 3

--- Frutzov uber račun.cpp
+++ Frutzov uber račun.cpp
@@ -18,8 +18,8 @@ USEFORM("unitSetPrinter.cpp", formSetPrinter);
 USEFORM("unitSetConfigFolder.cpp", formSetConfigFolder);
 USEFORM("unitLogo.cpp", formLogo);
 USEFORM("Forms\unitAdditionalOptions.cpp", formAdditionalOptions);
-USEFORM("unitCorruptSettings.cpp", formCorruptSettings);
 USEFORM("Forms\unitClose.cpp", formClose);
+USEFORM("Forms\unitCorruptSettings.cpp", formCorruptSettings);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -35,9 +35,9 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->CreateForm(__classid(TformPrintMessage), &formPrintMessage);
 		Application->CreateForm(__classid(TformOptions), &formOptions);
 		Application->CreateForm(__classid(TformOpenNew), &formOpenNew);
-		Application->CreateForm(__classid(TformCorruptSettings), &formCorruptSettings);
 		Application->CreateForm(__classid(TformAdditionalOptions), &formAdditionalOptions);
 		Application->CreateForm(__classid(TformClose), &formClose);
+		Application->CreateForm(__classid(TformCorruptSettings), &formCorruptSettings);
 		Application->Run();
 	}
 	catch (Exception &exception)
--- unitCorruptSettings.dfm
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b5740e3c07999f87645cf440d47d95d0acc2babc9878b7afadabcd9ddf56d1a3
-size 2984
